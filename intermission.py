ffb2f451b394bd88b99d29035477d9a16ec5077b
roasticle <mbommel@live.com>
2016-12-19 14:13:21 -0400

    Update intermission.py

--- intermission.py
+++ intermission.py
@@ -3,7 +3,6 @@ import minqlx
 class intermission(minqlx.Plugin):
 
     def __init__(self):
-        self.plugin_updater_url = "https://raw.githubusercontent.com/roasticle/minqlx-plugins/master/intermission.py"
         self.add_hook("game_end", self.handle_game_end)
 
     def handle_game_end(self, *args, **kwargs):
