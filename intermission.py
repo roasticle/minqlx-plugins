8f7cf6ff824ca37269ea51716044940a66f9e0ff
roasticle <mbommel@live.com>
2017-01-27 16:14:52 -0400

    Delete intermission.py

--- intermission.py
+++
@@ -1,9 +0,0 @@
-import minqlx
-
-class intermission(minqlx.Plugin):
-
-    def __init__(self):
-        self.add_hook("game_end", self.handle_game_end)
-
-    def handle_game_end(self, *args, **kwargs):
-        self.play_sound(self.get_cvar("qlx_intermissionSound"))
