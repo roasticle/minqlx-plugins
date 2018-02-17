97ce778d6b41174611deaaaf88f76fcdc4813d00
roasticle <mbommel@live.com>
2018-02-17 14:13:06 -0400

    Update idlespec.py

--- idlespec.py
+++ idlespec.py
@@ -1,1 +1,11 @@
 
+import minqlx
+
+class idlespec(minqlx.Plugin):
+    
+    def __init__(self):        
+        self.add_hook("player_inactivity_kick", self.handle_inactivity_kick)
+        
+    def handle_inactivity_kick(self, p):
+        p.put("spectator")
+        return minqlx.RET_STOP_EVENT
