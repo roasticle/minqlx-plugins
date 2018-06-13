8e3d2b69f8e13fd470bc9bac5e34ee2483b1a90a
roasticle <mbommel@live.com>
2018-06-12 23:05:01 -0300

    Update qltv.py

--- qltv.py
+++ qltv.py
@@ -10,6 +10,7 @@ class qltv(minqlx.Plugin):
         self.spec_timer()
 
         self.add_command("qltv", self.cmd_qltv)
+        self.add_command("qltvjoin", self.cmd_qltvjoin)
 
     @minqlx.delay(45)
     def spec_timer(self):
@@ -23,7 +24,7 @@ class qltv(minqlx.Plugin):
                 self.spec_index += 1
 
             for p in self.teams()['spectator']:
-                if int(self.db.get(PLAYER_KEY.format(p.steam_id) + ":qltv")) == 1:
+                if self.db.get(PLAYER_KEY.format(p.steam_id) + ":qltv") == "1":
                     minqlx.client_command(p.id, 'follow ' + str(sorted_players[self.spec_index].id))
 
             self.last_spec_steam_id = sorted_players[self.spec_index].steam_id
@@ -31,17 +32,22 @@ class qltv(minqlx.Plugin):
             self.spec_timer()
 
     def cmd_qltv(self, player, msg, channel):
-        current_qltv_setting = int(self.db.get(PLAYER_KEY.format(player.steam_id) + ":qltv"))
+        current_qltv_setting = self.db.get(PLAYER_KEY.format(player.steam_id) + ":qltv")
 
-        if not current_qltv_setting or current_qltv_setting == 0:
+        if not current_qltv_setting or current_qltv_setting == "0":
             current_qltv_setting = 0
             player.tell('^5***QTLV HAS BEEN ENABLED***')
 
-        if current_qltv_setting == 1:
+        if current_qltv_setting == "1":
             new_qltv_setting = 0
             player.tell('^5***QTLV HAS BEEN DISABLED***')
         else:
             new_qltv_setting = 1
 
         self.db.set(PLAYER_KEY.format(player.steam_id) + ":qltv", new_qltv_setting)
+
+    def cmd_qltvjoin(self, player, msg, channel):
+        self.db.set(PLAYER_KEY.format(player.steam_id) + ":qltv", 1)
+        minqlx.client_command(p.id, 'follow ' + str(sorted_players[self.spec_index].id))
+
 
