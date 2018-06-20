87ccbd3b236d7e99be3efaa9e141918a2f0d7d37
roasticle <mbommel@live.com>
2018-06-19 22:26:42 -0300

    Update qltv.py

--- qltv.py
+++ qltv.py
@@ -6,34 +6,36 @@ class qltv(minqlx.Plugin):
 
     def __init__(self):
         self.spec_index = 0
-        self.last_spec_steam_id = 0
+        self.last_spec_steam_id = None
         self.sorted_players = ""
 
         self.add_command("qltv", self.cmd_qltv)
         self.add_command("qltvjoin", self.cmd_qltvjoin)
 
         self.add_hook("game_start", self.handle_game_start)
+        self.add_hook("stats", self.handle_stats)
 
     def handle_game_start(self, data):
         self.spec_index = 0
-        self.last_spec_steam_id = 0
-        self.sorted_players = ""
         self.speccer()
 
-    @minqlx.delay(45)
-    def spec_timer(self):
-        self.speccer()
+    def handle_stats(self, stats):
+        if self.game is not None:
+            if self.game.state == "in_progress":
+                if stats['TYPE'] == "PLAYER_KILL":
+                    if self.game.red_score % 10 == 0:
+                        self.speccer()
 
-    @minqlx.delay(2)
+    @minqlx.delay(1)
     def speccer(self):
         if self.game.state == "in_progress":
             self.sorted_players = sorted(self.players(), key = lambda p: p.stats.score, reverse=True)
-            sorted_player_count = len(self.sorted_players)
 
-            if self.spec_index + 1 > sorted_player_count or self.spec_index == 3:
-                self.spec_index = 0
-            elif self.last_spec_steam_id == self.sorted_players[self.spec_index].steam_id:
-                self.spec_index += 1
+            if self.sorted_players[self.spec_index].steam_id == self.last_spec_steam_id:
+                if self.spec_index + 1 == 3:
+                    self.spec_index = 0
+                else:
+                    self.spec_index += 1
 
             for player in self.teams()['spectator']:
                 player_spec_setting = self.db.get(PLAYER_KEY.format(player.steam_id) + ":qltv")
@@ -43,9 +45,11 @@ class qltv(minqlx.Plugin):
                         minqlx.client_command(player.id, 'follow ' + str(self.sorted_players[self.spec_index].id))
 
             self.last_spec_steam_id = self.sorted_players[self.spec_index].steam_id
-            self.spec_index += 1
 
-        self.spec_timer()
+            if self.spec_index == 2:
+                self.spec_index = 0
+            else:
+                self.spec_index += 1
 
     def cmd_qltv(self, player, msg, channel):
         current_qltv_setting = self.db.get(PLAYER_KEY.format(player.steam_id) + ":qltv")
