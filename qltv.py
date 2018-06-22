c6d776c7eb1eaec59fd3cb3127cd7972594c4916
roasticle <mbommel@live.com>
2018-06-22 05:15:21 -0300

    Update qltv.py

--- qltv.py
+++ qltv.py
@@ -5,20 +5,27 @@ PLAYER_KEY = "minqlx:players:{}"
 class qltv(minqlx.Plugin):
 
     def __init__(self):
-        self.spec_index = self.kill_threshold_hit = 0
+        self.kill_threshold_hit = 0
         self.last_spec_steam_id = None
         self.sorted_players = ""
 
         self.add_command("qltv", self.cmd_qltv)
 
         self.add_hook("game_start", self.handle_game_start)
-        self.add_hook("new_game", self.handle_game_start)
+        self.add_hook("new_game", self.handle_new_game)
         self.add_hook("stats", self.handle_stats)
         self.add_hook("player_connect", self.handle_player_connect)
         self.add_hook("player_disconnect", self.handle_player_disconnect)
+        self.add_hook("team_switch_attempt", self.handle_team_switch_attempt)
 
+    @minqlx.delay(3)
     def handle_game_start(self, data):
-        self.spec_index = self.kill_threshold_hit = 0
+        self.kill_threshold_hit = 0
+        self.speccer()
+
+    @minqlx.delay(3)
+    def handle_new_game(self, *args, **kwargs):
+        self.kill_threshold_hit = 0
         self.speccer()
 
     def handle_stats(self, stats):
@@ -33,21 +40,10 @@ class qltv(minqlx.Plugin):
     def speccer(self):
         self.sorted_players = sorted(self.players(), key = lambda p: p.stats.score, reverse=True)
 
-        if self.sorted_players[self.spec_index].steam_id == self.last_spec_steam_id:
-            if self.spec_index + 1 == 3:
-                self.spec_index = 0
-            else:
-                self.spec_index += 1
-
         for player in self.teams()['spectator']:
             self.check_and_spec(player)
 
-        self.last_spec_steam_id = self.sorted_players[self.spec_index].steam_id
-
-        if self.spec_index == 2:
-            self.spec_index = 0
-        else:
-            self.spec_index += 1
+        self.last_spec_steam_id = self.sorted_players[0].steam_id
 
     def check_and_spec(self, player):
         player_spec_setting = self.db.get(PLAYER_KEY.format(player.steam_id) + ":qltv")
@@ -78,6 +74,9 @@ class qltv(minqlx.Plugin):
     def handle_player_connect(self, player):
         self.check_and_spec(player)
 
-    def handle_player_disconnect(self, player):
+    def handle_player_disconnect(self, player, reason):
         self.speccer()
 
+    def handle_team_switch_attempt(self, player, old_team, new_team):
+        if player.steam_id == self.last_spec_steam_id and new_team == "spectator":
+            self.speccer()
