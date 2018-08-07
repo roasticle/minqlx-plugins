11f5d459ba38458c9ba10614092571c398c6da31
roasticle <mbommel@live.com>
2018-08-06 21:17:40 -0300

    Update uberstats.py

--- uberstats.py
+++ uberstats.py
@@ -10,7 +10,12 @@ class uberstats(minqlx.Plugin):
         self.add_hook("game_start", self.handle_game_start)
         self.add_hook("game_end", self.handle_game_end)
 
-        self.weapon_accuracies = ["PLASMA", "ROCKET", "PROXMINE", "RAILGUN", "CHAINGUN", "NAILGUN", "GRENADE", "LIGHTNING", "SHOTGUN", "MACHINEGUN", "HMG", "BFG"]
+        self.weapons = ["PLASMA", "ROCKET", "PROXMINE", "RAILGUN", "CHAINGUN", "NAILGUN", "GRENADE", "LIGHTNING", "SHOTGUN", "MACHINEGUN", "HMG", "BFG", "GAUNTLET"]
+        self.weapon_sprees = ["PLASMORGASM", "ROCKET RENEGADE", "MINE MASTER", "RAIL RANGER", "CHAIN GANG", "GNARLY NAILER", "GRENADE GOON", "LIGHTNING LASHER", "SHOTGUN SAMURAI", "MACHINEGUN PECKER", "HMG HARASSER", "BFG BOSS", "GUANTLET GOD"]
+        self.kill_streak = {}
+        for weapon in self.weapons:
+            self.kill_streak[weapon] = {}
+
         self.outputted_accuracy_players = []
         self.kamikaze_stats = {}
         self.plasma_stats = {}
@@ -45,7 +50,7 @@ class uberstats(minqlx.Plugin):
         self.world_death_stats = {}
         self.most_world_deaths_names = []
         self.most_world_deaths = 0
-        
+
         self.most_dmg_per_kill_names = []
         self.most_dmg_per_kill = 0
 
@@ -62,14 +67,19 @@ class uberstats(minqlx.Plugin):
                                  player.stats.ping
                              )
                         )
-    
+
     def handle_stats(self, stats):
         if self.game is not None:
-            if self.game.state == "in_progress":
+            #if self.game.state == "in_progress":
+            if self.game.state != "in_progress":
                 if stats['TYPE'] == "PLAYER_DEATH":
-                    #remove player from plasma kill counter when they die
-                    if stats['DATA']['VICTIM']['NAME'] in self.plasma_stats:
-                        self.plasma_stats[stats['DATA']['VICTIM']['NAME']] = 0
+                    victim_name = stats['DATA']['VICTIM']['NAME']
+
+                    #remove player from kill streak counters when they die
+                    for weapon in self.weapons:
+                        if self.kill_streak[weapon]:
+                            if victim_name in self.kill_streak[weapon]:
+                                self.kill_streak[weapon][victim_name] = 0
 
                     #count player world deaths
                     if stats['DATA']['MOD'] in self.world_death_types:
@@ -78,17 +88,18 @@ class uberstats(minqlx.Plugin):
                             self.world_death_stats[victim_name] = 1
                         else:
                             self.world_death_stats[victim_name] += 1
-
-                elif stats['TYPE'] == "PLAYER_KILL" and stats['DATA']['MOD'] == "PLASMA":
+                elif stats['TYPE'] == "PLAYER_KILL" and stats['DATA']['MOD'] in self.weapons:
                     killer_name = stats['DATA']['KILLER']['NAME']
-                    if killer_name != stats['DATA']['VICTIM']['NAME']:
-                        if killer_name not in self.plasma_stats:
-                            self.plasma_stats[killer_name] = 1
-                        else:
-                            self.plasma_stats[killer_name] += 1
+                    weapon = stats['DATA']['MOD']
 
-                        if self.plasma_stats[killer_name] == 1:
-                            self.handle_plasma_stats(killer_name)
+                    if killer_name != stats['DATA']['VICTIM']['NAME']:
+                        if killer_name not in self.kill_streak[weapon]:
+                            self.kill_streak[weapon][killer_name] = 1
+                        else:
+                            self.kill_streak[weapon][killer_name] += 1
+
+                        if self.kill_streak[weapon][killer_name] == 1:
+                            self.handle_kill_streak(killer_name, weapon)
                 elif stats['TYPE'] == "PLAYER_KILL" and stats['DATA']['MOD'] == "KAMIKAZE":
                     killer_name = stats['DATA']['KILLER']['NAME']
                     if killer_name != stats['DATA']['VICTIM']['NAME']:
@@ -111,7 +122,7 @@ class uberstats(minqlx.Plugin):
                     #dont show if player is in spec, also handle multiple output bug as well
                     if player.team != "spectator" and player.steam_id not in self.outputted_accuracy_players:
                         accuracy_output = "^2YOUR ACCURACY:"
-                        for weapon in self.weapon_accuracies:
+                        for weapon in self.weapons:
                             weapon_shots = stats['DATA']['WEAPONS'][weapon]["S"]
                             weapon_hits = stats['DATA']['WEAPONS'][weapon]["H"]
                             if weapon_shots > 0:
@@ -147,7 +158,7 @@ class uberstats(minqlx.Plugin):
                     player_rail_accuracy = 0
 
                 player_nade_kills = stats['DATA']['WEAPONS']['GRENADE']['K']
-                player_pummels = stats['DATA']['WEAPONS']['GAUNTLET']['K']                
+                player_pummels = stats['DATA']['WEAPONS']['GAUNTLET']['K']
                 player_dmg_taken = stats['DATA']['DAMAGE']['TAKEN']
 
                 player_dmg_per_kill = 0
@@ -180,7 +191,7 @@ class uberstats(minqlx.Plugin):
                     self.most_damage = player_dmg
                 elif player_dmg == self.most_damage:
                     self.most_damage_names.append(player_name)
-                    
+
                 if not self.longest_spree:
                     self.longest_spree_names = [player_name]
                     self.longest_spree = player_longest_spree
@@ -188,8 +199,8 @@ class uberstats(minqlx.Plugin):
                     self.longest_spree_names = [player_name]
                     self.longest_spree = player_longest_spree
                 elif player_longest_spree == self.longest_spree:
-                    self.longest_spree_names.append(player_name)  
-                    
+                    self.longest_spree_names.append(player_name)
+
                 if not self.best_rail_accuracy_names:
                     self.best_rail_accuracy_names = [player_name]
                     self.best_rail_accuracy = player_rail_accuracy
@@ -220,7 +231,7 @@ class uberstats(minqlx.Plugin):
                     self.most_pummels = player_pummels
                 elif player_pummels == self.most_pummels:
                     self.most_pummels_names.append(player_name)
-                    
+
                 if not self.most_dmg_taken_names:
                     self.most_dmg_taken_names = [player_name]
                     self.most_dmg_taken = player_dmg_taken
@@ -228,8 +239,8 @@ class uberstats(minqlx.Plugin):
                     self.most_dmg_taken_names = [player_name]
                     self.most_dmg_taken = player_dmg_taken
                 elif player_dmg_taken == self.most_dmg_taken:
-                    self.most_dmg_taken_names.append(player_name)    
-                    
+                    self.most_dmg_taken_names.append(player_name)
+
                 if not self.most_dmg_per_kill_names:
                     self.most_dmg_per_kill_names = [player_name]
                     self.most_dmg_per_kill = player_dmg_per_kill
@@ -237,7 +248,7 @@ class uberstats(minqlx.Plugin):
                     self.most_dmg_per_kill_names = [player_name]
                     self.most_dmg_per_kill = player_dmg_per_kill
                 elif player_dmg_per_kill == self.most_dmg_per_kill:
-                    self.most_dmg_per_kill_names.append(player_name)       
+                    self.most_dmg_per_kill_names.append(player_name)
 
     @minqlx.delay(2)
     @minqlx.thread
@@ -285,8 +296,8 @@ class uberstats(minqlx.Plugin):
                     if len(self.best_rail_accuracy_names) > 1 and len(self.best_rail_accuracy_names) - 1 != i:
                         stats_output += ", "
                 stats_output += "^2 - {:0.2f} percent rail accuracy ({} hits / {} shots)".format(self.best_rail_accuracy, self.best_rail_hits, self.best_rail_shots)
-                self.msg(stats_output)      
-            
+                self.msg(stats_output)
+
             if self.most_nade_kills > 0:
                 stats_output = "^3PINEAPPLE POWER: "
                 for i, player_name in enumerate(self.most_nade_kills_names):
@@ -294,7 +305,7 @@ class uberstats(minqlx.Plugin):
                     if len(self.most_nade_kills_names) > 1 and len(self.most_nade_kills_names) - 1 != i:
                         stats_output += ", "
                 stats_output += "^2 - {} grenade frags".format(self.most_nade_kills)
-                self.msg(stats_output)    
+                self.msg(stats_output)
 
             time.sleep(2)
 
@@ -346,12 +357,12 @@ class uberstats(minqlx.Plugin):
                 self.msg(stats_output)
 
     @minqlx.delay(8)
-    def handle_plasma_stats(self, player_name):
-        if self.plasma_stats[player_name] >= 5:
-            self.play_sound("sound/uberstats/plasmorgasm.ogg")
-            self.center_print("{}^6 PLASMORGASM".format(player_name))
-            self.msg("{} ^6PLASMORGASM: ^7({} plasma frags in 8s)".format(player_name, self.plasma_stats[player_name]))
-        self.plasma_stats[player_name] = 0
+    def handle_kill_streak(self, player_name, weapon):
+        if int(self.kill_streak[weapon][player_name]) >= 4:
+            self.play_sound("sound/uberstats/{}}.ogg".format(weapon.lower()))
+            self.center_print("{}^1 {}".format(player_name, self.weapon_sprees[self.weapons.index(weapon)]))
+            self.msg("{} ^1{}: ^2({} {} frags in 8s)".format(player_name, self.weapon_sprees[self.weapons.index(weapon)], self.kill_streak[weapon][player_name], weapon))
+        self.kill_streak[weapon][player_name] = 0
 
     @minqlx.delay(5)
     def handle_kamikaze_stats(self, player_name):
@@ -390,12 +401,15 @@ class uberstats(minqlx.Plugin):
         self.world_death_stats = {}
         self.most_world_deaths_names = []
         self.most_world_deaths = 0
-        
+
         self.most_dmg_per_kill_names = []
         self.most_dmg_per_kill = 0
 
         self.kamikaze_stats = {}
         self.plasma_stats = {}
+        for weapon in self.weapons:
+            self.kill_streak[weapon] = {}
+
         self.outputted_accuracy_players = []
 
     def ordinal(self, value):
